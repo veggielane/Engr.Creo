7b23b24eaec0c7c46e43a426922b764e029cf8c4
veggielane <chris@lane.org.uk>
2014-11-20 20:58:32 +0000

    Cleaned up file paths

--- Engr.Creo.JLink/Engr.Creo.Jlink.LaunchPane/src/LaunchPane.java
+++ Engr.Creo.JLink/Engr.Creo.Jlink.LaunchPane/src/LaunchPane.java
@@ -1,30 +1,27 @@
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-
-import com.ptc.cipjava.jxthrowable;
 import com.ptc.pfc.pfcGlobal.*;
 import com.ptc.pfc.pfcSession.*;
 
 public class LaunchPane {
 
-    public static void start() throws jxthrowable, IOException {
-
-
+    public static void start(){
+        try {
             Session session = pfcGlobal.GetProESession();
             Properties prop = new Properties();
             String propFileName = "LaunchPane.properties";
             InputStream inputStream = LaunchPane.class.getClassLoader().getResourceAsStream(propFileName);
-
             prop.load(inputStream);
             if (inputStream == null) {
                 throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
             }
             String icon = prop.getProperty("icon");
-            session.UIShowMessageDialog(icon,null);
             if (icon != null && icon.isEmpty()) icon = null;
             session.NavigatorPaneBrowserAdd(prop.getProperty("name"), icon, prop.getProperty("url"));
+        } catch (Throwable x) {
+
+        }
     }
 
     public static void stop() {
